ae3d1aa5b7606544a0b24f41a96ed6d22fa313c9
Javier <javierenriquemendozagomez@gmail.com>
2023-08-02 12:16:13 +0100

    Add files via upload

--- tests/vitest/fibonacci.test.ts
+++ tests/vitest/fibonacci.test.ts
@@ -1,8 +1,14 @@
-/*
-Write your tests here!
+import { describe, it } from 'vitest';
+import { fibonacciRecursive } from '../../fibonacci';
 
-Remember to implement these tests:
-  assert.equal(fibonacciRecursive(40), 102334155);
-  assert.equal(fibonacciRecursive(43), 433494437);
-  assert.equal(fibonacciRecursive(45), 1134903170);
-*/
+describe('fibonacciRecursive', () => {
+  it.concurrent('concurrent test 1', async ({ expect }) => { 
+    expect(fibonacciRecursive(40)).toBe(102334155);
+  });
+  it.concurrent('concurrent test 2', async ({ expect }) => { 
+    expect(fibonacciRecursive(43)).toBe(433494437);
+  });
+  it.concurrent('concurrent test 3', async ({ expect }) => { 
+    expect(fibonacciRecursive(45)).toBe(1134903170);
+  });
+});
