ae2ceea537a5fc48c2d12b2a68d8848e9555865e
tanimaroy2012 <45754357+tanimaroy2012@users.noreply.github.com>
2023-08-02 12:04:45 +0100

    added stuff for swc

--- tests/jest-swc/fibonacci.test.ts
+++ tests/jest-swc/fibonacci.test.ts
@@ -6,3 +6,19 @@ Remember to implement these tests:
   assert.equal(fibonacciRecursive(43), 433494437);
   assert.equal(fibonacciRecursive(45), 1134903170);
 */
+import { fibonacciRecursive } from '../../fibonacci';
+
+describe('fibonacciRecursive', () => {
+  it('should throw an error for invalid input', () => {
+    expect(fibonacciRecursive(40)).toBe(102334155);
+  });
+
+  it('should return the correct value for valid input', () => {
+    expect(fibonacciRecursive(43)).toBe(433494437);
+  });
+
+  it('should return the correct value for valid input', () => {
+    expect(fibonacciRecursive(45)).toBe(1134903170);
+  });
+});
+
