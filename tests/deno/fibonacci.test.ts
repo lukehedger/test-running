69616c67037a41deccbf5c3bc77643c7582f0097
Clara Villa <52048170+claravilla@users.noreply.github.com>
2023-08-02 12:12:21 +0100

    Update fibonacci.test.ts

--- tests/deno/fibonacci.test.ts
+++ tests/deno/fibonacci.test.ts
@@ -6,3 +6,19 @@ Remember to implement these tests:
   assert.equal(fibonacciRecursive(43), 433494437);
   assert.equal(fibonacciRecursive(45), 1134903170);
 */
+
+import {fibonacciRecursive} from '../../fibonacci.ts';
+// @ts-ignore Import module
+import { assertEquals } from 'https://deno.land/std@0.196.0/testing/asserts.ts';
+
+Deno.test("fibonacciRecursive(40) should return 102334155", () => {
+  assertEquals(fibonacciRecursive(40), 102334155);
+});
+
+Deno.test("fibonacciRecursive(43) should return 433494437", () => {
+  assertEquals(fibonacciRecursive(43), 433494437);
+});
+
+Deno.test("fibonacciRecursive(45) should return 1134903170", () => {
+  assertEquals(fibonacciRecursive(45), 1134903170);
+});
