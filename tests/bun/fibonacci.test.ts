7c31835d04a4c17a3659135accaa3d9165a7143c
Daniel Malcolm <daniel.malcolm@lego.com>
2023-08-02 11:17:00 +0100

    tidy up bun test

--- tests/bun/fibonacci.test.ts
+++ tests/bun/fibonacci.test.ts
@@ -1,25 +1,9 @@
 import {expect, test, describe} from "bun:test";
 import {fibonacciRecursive} from "../../fibonacci";
 
-test("2 + 2", () => {
-  expect(2 + 2).toBe(4);
-});
-
-/*
-Write your tests here!
-
-Remember to implement these tests:
-  assert.equal(fibonacciRecursive(40), 102334155);
-  assert.equal(fibonacciRecursive(43), 433494437);
-  assert.equal(fibonacciRecursive(45), 1134903170);
-*/
-
 describe("fibonacciRecursive", () => {
   test("40 should return 102334155", () => {
     expect(fibonacciRecursive(40)).toBe(102334155);
-  });
-  test("50 should return 12586269025", () => {
-    expect(fibonacciRecursive(50)).toBe(12586269025);
   });
   test("43 should return 433494437", () => {
     expect(fibonacciRecursive(43)).toBe(433494437);
